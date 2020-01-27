66dd25231908310a1a7dfca835434d0174eee177
Austin Johnson <technicallyaustin@gmail.com>
2020-01-27 20:14:12 +0000

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+  attr_accessor :hello, :raise_hand 
+  def iniitialize()
+end
+
+def hello
+ @msg =  "Hey there! I'm so excited to learn stuff."
+ puts @msg 
+end
+def raise_hand
+  puts "Pick me!"
+end
+
+end 
